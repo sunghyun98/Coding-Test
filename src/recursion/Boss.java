1af3d73b41d572b3e4ea68364b55ce3e1daa0319
sunghyun <a91078933@gmail.com>
2023-05-05 23:29:40 +0900

    s

---
+++ src/recursion/Boss.java
@@ -0,0 +1,24 @@
+package recursion;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 농부 상근이는 마당에 심기 위한 나무 묘목 n개를 구입했다.
+ * 묘목 하나를 심는데 걸리는 시간은 1일이고,
+ * 상근이는 각 묘목이 다 자라는데 며칠이 걸리는지 정확하게 알고 있다.
+ * 상근이는 마을 이장님을 초대해 자신이 심은 나무를 자랑하려고 한다.
+ * 이장님을 실망시키면 안되기 때문에, 모든 나무가 완전히 자란 이후에 이장님을 초대하려고 한다.
+ * 즉, 마지막 나무가 다 자란 다음날 이장님을 초대할 것이다.
+ * 상근이는 나무를 심는 순서를 신중하게 골라 이장님을 최대한 빨리 초대하려고 한다. 이장님을 며칠에 초대할 수 있을까?
+ *
+ * 첫째 줄에는 묘목의 수 N (1 ≤ N ≤ 100,000)이 주어진다.
+ * 둘째 줄에는 각 나무가 다 자라는데 며칠이 걸리는지를 나타낸 ti가 주어진다. (1 ≤ ti ≤ 1,000,000)
+ */
+public class Boss {
+    int arr[];
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int
+    }
+}
--- src/recursion/ZOAC.java
+++ src/recursion/ZOAC.java
@@ -6,13 +6,13 @@ import java.io.InputStreamReader;
 
 /**
  * 2018년 12월, 처음 시작하게 된 ZOAC의 오프닝을 맡은 성우는 누구보다 화려하게 ZOAC를 알리려 한다.
- *
+ * <p>
  * 앞 글자부터 하나씩 보여주는 방식은 너무 식상하다고 생각한 성우는 문자열을 보여주는 새로운 규칙을 고안해냈다!
- *
+ * <p>
  * 규칙은 이러하다. 아직 보여주지 않은 문자 중 추가했을 때의 문자열이 사전 순으로 가장 앞에 오도록 하는 문자를 보여주는 것이다.
- *
+ * <p>
  * 예를 들어 ZOAC를 보여주고 싶다면, A → AC → OAC → ZOAC 순으로 보여주면 된다.
- *
+ * <p>
  * 바쁜 성우를 위하여 이 규칙대로 출력해주는 프로그램을 작성하시오.
  */
 public class ZOAC {
@@ -23,40 +23,36 @@ public class ZOAC {
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
+        input = br.readLine();
 
-        input = br.readLine(); //입력 받고
-        visited = new boolean[input.length()]; //입력 받은 값만금 배열 생성
-
+        visited = new boolean[input.length()];
         zoac(0, input.length() - 1);
 
         br.close();
         System.out.println(sb.toString());
+
     }
 
     private static void zoac(int left, int right) throws IOException {
-        if (left > right) return;
+        if(left > right)return; //재귀 종료
 
-        int idx = left;
+        int index = left;
 
-        // left와 right 사이에 있는 글자중 사전식 순서가 가장 낮은 글자를 찾는다.(idx)
-        for (int i = left; i <= right; i++) {
-            if (input.charAt(idx) > input.charAt(i)) {
-                idx = i;
+        for (int i = left; i <= right; i++){
+            if (input.charAt(index) > input.charAt(i)){
+                index = i;
             }
         }
-        visited[idx] = true;
+        visited[index] = true;
 
-        for (int i = 0; i < input.length(); i++) {
+        for (int i = 0; i < input.length(); i++){
             if (visited[i]) {
                 sb.append(input.charAt(i));
             }
-        }
-        sb.append("\n");
+        }sb.append('\n');
 
-        // 여기도 순서가 안맞으면 틀렸습니다를 확인할 수 있다.
-        // 반드시 이 dfs 순서로 재귀를 구현해야 한다.
-        // 그래야 사전식 순서가 맞춰진다.
-        zoac(idx + 1, right);
-        zoac(left, idx  - 1);
+        zoac(index+1, right);
+        zoac(left,index-1);
+
     }
 }
