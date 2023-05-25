29dbfd1b2b44a7e57083112595cbc6ed0f4f4342
sunghyun <a91078933@gmail.com>
2023-05-25 22:46:47 +0900

    백준 문제 2444

--- src/Beakjoon/B_2444.java
+++ src/Beakjoon/B_2444.java
@@ -25,16 +25,23 @@ import java.util.Scanner;
  */
 public class B_2444 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
 
-        int N = sc.nextInt();
-        int a = 1;
-
-        for (int i = 0; i < N; i++ ){
-            for (int j = 0; j < a; j++ ){
+        for(int i = 1; i <= N ; i++) {
+            for(int j = 0; j < N-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i*2-1; j++)
                 System.out.print("*");
-            }
-            a = a + 2;
+            System.out.println();
+        }
+
+        for(int i = N-1; i >= 0 ; i--) {
+            for(int j = 0; j < N-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i*2-1; j++)
+                System.out.print("*");
             System.out.println();
         }
     }
