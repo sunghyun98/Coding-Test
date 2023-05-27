7ff5bba11004169c8c0fd739255d32cb02a6fa86
sunghyun <a91078933@gmail.com>
2023-05-27 23:45:47 +0900

    백준 문제 10798 숫자 카드 시간 초과됨 > 이진탐색

--- src/Beakjoon/B_10798.java
+++ src/Beakjoon/B_10798.java
@@ -1,6 +1,9 @@
 package Beakjoon;
 
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * 문제
@@ -22,35 +25,48 @@ import java.util.Scanner;
  *
  */
 public class B_10798 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
+    static int arr[];
 
-        int M = sc.nextInt();
-        int arrM[] = new int[M];
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i < M; i++){
-            arrM[i] = sc.nextInt();
+        for(int i = 0 ; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        // 숫자 카드 오름차순 정렬
+        Arrays.sort(arr);
+
+        m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0 ; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            // 이분 탐색을 해서 해당 숫자가 있는 경우
+            if(binarySearch(num)) bw.write("1 ");
+                // 이분 탐색을 해서 해당 숫자가 없는 경우
+            else bw.write("0 ");
         }
 
-        int N = sc.nextInt();
-        int arrN[] = new int[N];
+        bw.close();
+        br.close();
+    }
+    private static boolean binarySearch(int num) {
+        int leftIndex = 0;
+        int rightIndex = n - 1;
 
-        for(int i = 0; i < N; i++){
-            arrN[i] = sc.nextInt();
+        while(leftIndex <= rightIndex){
+            int midIndex = (leftIndex + rightIndex) / 2;
+            int mid = arr[midIndex];
+
+            if(num < mid) rightIndex = midIndex - 1;
+            else if(num > mid) leftIndex = midIndex + 1;
+            else return true;
         }
-
-            for(int i = 0; i < N; i++){
-                int a = 0;
-                for (int j = 0; j < M; j++){
-                    if (arrN[i] == arrM[j]){
-                        a = 1;
-                        continue;
-                    }
-                }
-                System.out.print(a+" ");
-            }
-
-
-
+        return false;
     }
 }
