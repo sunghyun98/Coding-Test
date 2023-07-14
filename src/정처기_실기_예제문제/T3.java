7fc8e1942b999be95291764f37d0451cf883fff8
sunghyun <a91078933@gmail.com>
2023-07-14 21:47:39 +0900

    1

---
+++ src/정처기_실기_예제문제/T3.java
@@ -0,0 +1,15 @@
+package 정처기_실기_예제문제;
+
+public class T3 {
+    public static void main(String[] args) {
+        char[] ch = new char[5];
+        String str = "abcde";
+        int[] num = {1, 2, 3, 4, 5};
+
+        System.out.println(ch.length);
+        System.out.println(str.length());
+        System.out.println(num.length);
+        System.out.println(str.length());
+
+    }
+}
