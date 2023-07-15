6cf7e7c153377be055e6d4ad49f0cee7481ceea4
sunghyun <a91078933@gmail.com>
2023-07-15 22:35:57 +0900

    1

---
+++ src/정처기_실기_예제문제/Static.java
@@ -0,0 +1,17 @@
+package 정처기_실기_예제문제;
+
+class Static {
+    public int a = 20;
+    static int b = 0;
+
+    public static void main(String[] args) {
+        int a;
+        a = 10;
+        Static.b = a;
+        Static st = new Static();
+        System.out.println(Static.b++);
+        System.out.println(st.b);
+        System.out.println(a);
+        System.out.println(st.a);
+    }
+}
