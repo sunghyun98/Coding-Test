1df778acf626e1210f925d1a30949c95bc6bc2ff
sunghyun <a91078933@gmail.com>
2023-07-16 20:44:39 +0900

    1

---
+++ src/정처기_실기_예제문제/T5.java
@@ -0,0 +1,13 @@
+package 정처기_실기_예제문제;
+
+public class T5 {
+
+    public static void main(String[] args) {
+        int a, hap = 0;
+        for (a = 0; a < 10; a++, hap += a){
+            System.out.println("a = " + a);
+            System.out.println("hap = " + hap);
+        }
+
+    }
+}
