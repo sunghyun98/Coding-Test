3b146aa05f0bed422961fed509d4bf84e9c35eaf
sunghyun <a91078933@gmail.com>
2023-10-11 18:23:09 +0900

    시간 계산

---
+++ src/CodingTest/Beakjoon/Conditional/B_2525.java
@@ -0,0 +1,28 @@
+package CodingTest.Beakjoon.Conditional;
+import java.util.Scanner;
+/*
+첫째 줄에는 현재 시각이 나온다.
+현재 시각은 시 A (0 ≤ A ≤ 23) 와 분 B (0 ≤ B ≤ 59)가 정수로 빈칸을 사이에 두고 순서대로 주어진다.
+ 두 번째 줄에는 요리하는 데 필요한 시간 C (0 ≤ C ≤ 1,000)가 분 단위로 주어진다.
+ */
+
+public class B_2525 {t
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        int C = in.nextInt();
+
+        int min = 60 * A + B;   // 시 -> 분
+        min += C;
+
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+
+        System.out.println(hour + " " + minute);
+
+    }
+}
