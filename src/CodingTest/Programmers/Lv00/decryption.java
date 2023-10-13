d8020ac33fd21324ec33bcfe486c77b436177a19
sunghyun <a91078933@gmail.com>
2023-10-13 23:15:20 +0900

    미완

---
+++ src/CodingTest/Programmers/Lv00/decryption.java
@@ -0,0 +1,11 @@
+package CodingTest.Programmers.Lv00;
+
+import java.util.Scanner;
+
+public class decryption {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
