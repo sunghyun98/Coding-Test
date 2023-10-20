d67a1c6f1d55176589590707cdc6f90de778aa2b
sunghyun <a91078933@gmail.com>
2023-10-20 20:06:46 +0900

    프로그래머스 할머니 편지

---
+++ src/CodingTest/Programmers/Lv00/Letter.java
@@ -0,0 +1,19 @@
+package CodingTest.Programmers.Lv00;
+
+import java.util.Scanner;
+
+public class Letter {
+    public static int solution(String message) {
+        int answer = 0;
+        answer = message.length() * 2;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("편지 내용 ");
+        String message = sc.next();
+        System.out.println(solution(message));
+    }
+}
