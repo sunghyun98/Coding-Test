45aa19cf939e62cacc9b3bf6175d3335f860227d
sunghyun <a91078933@gmail.com>
2023-10-20 23:47:22 +0900

    프로그래머스 문자열안에 문자열

---
+++ src/CodingTest/Programmers/Lv00/Char_in.java
@@ -0,0 +1,22 @@
+package CodingTest.Programmers.Lv00;
+
+public class Char_in {
+    public static void main(String[] args) {String str1 = "asdfzxcv";
+        String str2 = "sd3f";
+
+        int j = 0;
+        int answer = 2;
+
+        for (int i = 0; i < str1.length(); i++){
+            if (str1.charAt(i)==str2.charAt(j)){
+                j++;
+                if (str2.length()== j){
+                    answer = 1;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(q);
+    }
+}
