1af379cce4e51c2040c7537d7654fdeb4de172e3
sunghyun <a91078933@gmail.com>
2023-10-24 23:58:42 +0900

    테스트

--- src/CodingTest/Programmers/Lv00/ArrCut.java
+++ src/CodingTest/Programmers/Lv00/ArrCut.java
@@ -1,9 +1,13 @@
 package CodingTest.Programmers.Lv00;
 
+import java.util.Arrays;
+
 public class ArrCut {
     public static void main(String[] args) {
         String [] sr= {"dsd", "ds"};
         int[] ar = {1,4,5};
         System.out.println(sr[1].length());
+        Arrays.sort(ar);
+
     }
 }
