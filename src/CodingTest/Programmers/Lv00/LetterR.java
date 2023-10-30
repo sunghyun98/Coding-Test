47762d984d13396bf6f67ef072a9d55395f6396d
sunghyun <a91078933@gmail.com>
2023-10-30 22:45:34 +0900

    프로그래머스 0레벨

---
+++ src/CodingTest/Programmers/Lv00/LetterR.java
@@ -0,0 +1,22 @@
+package CodingTest.Programmers.Lv00;
+
+import java.util.Arrays;
+
+public class LetterR {
+    public int[] solution(String my_string) {
+        int[] answer = new int[100];
+        for(int i = 0; i < my_string.length(); i++){
+            if(my_string.charAt(i) >= 0 && my_string.charAt(i) <10){
+                answer[i] = Character.getNumericValue(my_string.charAt(i));
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int a = 'A'; //65
+        int b = 'a'; //97
+
+
+        System.out.println(b-a);
+    }
+}
---
+++ src/CodingTest/Programmers/Lv00/N.java
@@ -0,0 +1,10 @@
+package CodingTest.Programmers.Lv00;
+
+import java.util.Arrays;
+
+public class N {
+    public static void main(String[] args) {
+
+
+    }
+}
