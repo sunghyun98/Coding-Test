811278adb20c610144d86de8a46390f7b1c11da8
sunghyun <a91078933@gmail.com>
2023-11-02 22:21:38 +0900

    프로그래머스 0레벨

--- src/CodingTest/Programmers/Lv00/LetterR.java
+++ src/CodingTest/Programmers/Lv00/LetterR.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 public class LetterR {
     public int[] solution(String my_string) {
         int[] answer = new int[100];
-        for(int i = 0; i < my_string.length(); i++){
-            if(my_string.charAt(i) >= 0 && my_string.charAt(i) <10){
+        for(int i = 0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) >= 0 && my_string.charAt(i) < 10) {
                 answer[i] = Character.getNumericValue(my_string.charAt(i));
             }
+
         }
         return answer;
     }
