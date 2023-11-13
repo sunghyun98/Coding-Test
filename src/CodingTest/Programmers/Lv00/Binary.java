1599d004118e325d87789c4709a329dd440becd0
sunghyun <a91078933@gmail.com>
2023-11-13 22:22:19 +0900

    프로그래머스 0레벨 2진수 계산

---
+++ src/CodingTest/Programmers/Lv00/Binary.java
@@ -0,0 +1,16 @@
+package CodingTest.Programmers.Lv00;
+//toBinaryString
+public class Binary {
+    public static void main(String[] args) {
+        String answer = "";
+        String bin1 = "10";
+        String bin2 = "11";
+
+        int num1 = Integer.parseInt(bin1, 2);
+        int num2 = Integer.parseInt(bin2, 2);
+        int res = num1+num2;
+        answer = Integer.toBinaryString(res);
+        System.out.println(answer);
+        }
+    }
+
