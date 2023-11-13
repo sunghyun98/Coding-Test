15fe75e83a8513bdc4ca507ae640cde623e77bb2
sunghyun <a91078933@gmail.com>
2023-11-13 16:47:15 +0900

    프로그래머스 0레벨 중되지 않은 문자 찾기

---
+++ src/CodingTest/Programmers/Lv00/Ctrl.java
@@ -0,0 +1,18 @@
+package CodingTest.Programmers.Lv00;
+
+public class Ctrl {
+    public static void main(String[] args) {
+       String s = "1 2 Z 3";
+        int answer = 0;
+        String[] ctrl = s.split(" ");
+
+        for(int i = 0; i < ctrl.length; i++){
+            if(ctrl[i].equals("Z")){
+                answer -= Integer.parseInt(ctrl[i-1]);
+            }else{
+                answer += Integer.parseInt(ctrl[i]);
+            }
+        }
+        System.out.println(answer);;
+    }
+}
