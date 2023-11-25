d8fad606d282b3140e4ba250df0952f2a1759d40
sunghyun <a91078933@gmail.com>
2023-11-25 23:23:04 +0900

    프로그래머스 0레벨 배열자르기

--- src/CodingTest/Programmers/Lv00/ArrCut02.java
+++ src/CodingTest/Programmers/Lv00/ArrCut02.java
@@ -4,18 +4,9 @@ public class ArrCut02 {
     public static void main(String[] args) {
         String my_str = "abc1Addfggg4556b";
         int n = 6;
-        int m1 = 0;
-        String m = "";
-        int a = 0;
-        a = my_str.length()/n;
 
-        String answer[] = new String[my_str.length()/6];
-        for (int j = 0; j < my_str.length()/6; j++){
+        
 
-
-        }
-        //0 1 2 3 4 5 / 6 7 8 9 10 11 /
-        System.out.println(answer.toString());
 
     }
 }
