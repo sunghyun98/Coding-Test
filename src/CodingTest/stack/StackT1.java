c691a9dede70754140d9ef33bbf64e14f7dddeac
sunghyun <a91078933@gmail.com>
2024-03-17 00:31:32 +0900

    StackT1.java

---
+++ src/CodingTest/stack/StackT1.java
@@ -0,0 +1,26 @@
+package CodingTest.stack;
+
+
+    public class StackT1 {
+        int top;    //인덱스
+        int size;    //스택 배열의 크기
+        int [] stack;
+        public StackT1(int size) {
+            this.size = size;
+            stack = new int[size];
+            top = -1;
+        }
+
+        public void push(int item) {
+            stack[++top] = item;
+            System.out.println(stack[top] + " Push!");
+        }
+        public void pop() {
+            System.out.println(stack[top] + " Pop!");
+            int pop = stack[top];
+            stack[top--] = 0;
+        }
+        public void peek() {
+            System.out.println(stack[top] + " Peek!");
+        }
+    }
