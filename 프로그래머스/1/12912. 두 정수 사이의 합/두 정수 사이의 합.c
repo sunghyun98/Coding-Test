45f0775ae1524315d49269f2c5ee66e9102c0293
sunghyun <75673649+sunghyun98@users.noreply.github.com>
2024-09-13 23:48:33 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    if(a < b){
+        for(int i = a; i <= b; i++){
+            answer += i;
+        }
+    }else if(a > b){
+        for(int i = b; i <= a; i++){
+            answer += i;
+        }
+    }else
+        answer = a;
+    
+    return answer;
+}
