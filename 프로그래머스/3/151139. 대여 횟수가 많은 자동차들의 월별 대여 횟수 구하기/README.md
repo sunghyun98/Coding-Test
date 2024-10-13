cf217d8809c025805f58a441b055c172547ce258
sunghyun <75673649+sunghyun98@users.noreply.github.com>
2024-10-13 22:03:50 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기 - 151139 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 08월 14일 22:39:31
+2024년 10월 13일 22:03:48
 
 ### 문제 설명
 
--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,16 +1,6 @@
-
-SELECT  TO_CHAR(START_DATE, 'mm')*1 MONTH, CAR_ID,  COUNT(*) RECORDS
-FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY A
-WHERE   1=1
-        AND TO_CHAR(START_DATE,'YYYYMM') BETWEEN '202208' AND '202210'
-        AND CAR_ID IN   (
-                        SELECT  CAR_ID
-                        FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY A
-                        WHERE   1=1
-                                AND TO_CHAR(START_DATE,'YYYYMM') BETWEEN '202208' AND '202210'
-                        GROUP BY CAR_ID
-                        HAVING   COUNT(*) > 4
-                        )
-GROUP BY    TO_CHAR(START_DATE, 'mm'), CAR_ID
-HAVING      COUNT(*) <> 0
-ORDER BY    1 ASC, 2 DESC
+SELECT month(start_date) as month, car_id, count(history_id) as records
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+where start_date>="2022-08-01" and start_date<"2022-11-01" and car_id in (select car_id from CAR_RENTAL_COMPANY_RENTAL_HISTORY where start_date>="2022-08-01" and start_date<"2022-11-01" group by car_id having count(history_id)>4) 
+group by month, car_id
+having records >0
+order by month, car_id desc
